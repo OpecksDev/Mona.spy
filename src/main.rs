48977d0a7371166f0cfe090a862cec98cba8674a
Mateus de Castro <samuraiexx@gmail.com>
2020-12-22 20:56:20 -0300

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+extern crate rocket_contrib;
+
+use rocket_contrib::json::Json;
+use std::env;
+use types::wiki::{PromotionalCodes, WikiError, WikiResource};
+
+mod types;
+
+#[get("/promotionalCodes")]
+async fn promotional_codes() -> Json<Result<PromotionalCodes, WikiError>> {
+    Json(PromotionalCodes::get_wiki_resource().await)
+}
+
+#[launch]
+fn rocket() -> rocket::Rocket {
+    let port = env::var("PORT")
+        .map_or("8080".to_owned(), |x| x)
+        .parse::<u16>()
+        .unwrap();
+
+    let figment = rocket::Config::figment().merge(("port", port));
+    // .merge(("address", "0.0.0.0"));
+    rocket::custom(figment).mount("/", routes![promotional_codes])
+}
